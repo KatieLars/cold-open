40c2edb0353731d334a500747861adb5c80c6d13
github username <github email address>
2017-12-26 20:33:38 -0600

    Creating rspecs user_model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,20 +1,19 @@
 require 'rails_helper'
+require 'pry'
 
 RSpec.describe User, type: :model do
   before(:each) do
-    @user = User.first
-
+    @user = User.create(
+      username: "admin",
+      password: "admin",
+      email: "admin@gmail.com"
+    )
+binding.pry
   end
 
-  it "has many carts" do
-
-    @user.carts.create
-    expect(@user.carts.count).to eq(1)
-  end
-
-  it "has a current_cart" do
-    @user.current_cart = @user.carts.create
-    expect(@user.current_cart).to be_a(Cart)
+  it "has many items" do
+    @user.items.create
+    expect(@user.items.count).to eq(1)
   end
 
 end
