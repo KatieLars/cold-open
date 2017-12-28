d038582f0815b46674445102901328c57c23be03
github username <github email address>
2017-12-28 16:49:18 -0600

    Homepage css

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -4,4 +4,8 @@ class WelcomeController < ApplicationController
     #sign in, sign up, or reroutes to user homepage if already logged in
   end
 
+  def about
+    #links to about page
+  end
+
 end
