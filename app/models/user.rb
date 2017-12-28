1af1c6d2dcbe95f0fb966909a07c3e9d26aef556
github username <github email address>
2017-12-28 15:23:14 -0600

    creating user self.from_omniauth

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,16 @@ class User < ApplicationRecord
   has_many :freezers, -> { distinct }, through: :items
   has_many :notes #wired
 
+  def self.from_omniauth(auth)
+    where(provider: auth.provider, uid: auth.uid).first_or_initialize.tap do |user|
+      #user.provider = auth.provider
+      user.email = auth.info.email
+      user.uid = auth.uid
+      user.username = auth.info.name
+      #user.oauth_token = auth.credentials.token
+      #user.oauth_expires_at = Time.at(auth.credentials.expires_at)
+      user.save!
+    end
+  end
+
 end
--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,10 +2,10 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   provider :google_oauth2, ENV['GOOGLE_CLIENT_ID'], ENV['GOOGLE_CLIENT_SECRET'],
     {
       name: 'google',
-      scope: 'email, profile, image'
+      scope: 'email, profile'
     }
   provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET'],
     {
-      scope: 'email, profile, image'
+      scope: 'email, profile'
     }
 end
