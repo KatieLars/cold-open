9abb1752b30ea74cd61e6edaea2695377a2f0db7
github username <github email address>
2017-12-28 17:18:38 -0600

    custom nested routes

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,10 @@ Rails.application.routes.draw do
 
   resources :users, only: [:home] do
     resources :freezers, only: [:index, :new, :edit, :show]
-    resources :items, only: [:index, :new, :edit, :show, :expiring_this_week, :expiring_this_month, :expired]
+    resources :items, only: [:index, :new, :edit, :show]
+    get "/items/expiring_this_month" => "items#expiring_this_month", as: 'items_expire_month'
+    get "/items/expiring_this_week" => "items#expiring_this_week", as: 'items_expire_week'
+    get "/items/expired" => "items#expired", as: 'items_expired'
   end
 
   get '/users/:id/home' => 'users#home', as: 'user_home'
