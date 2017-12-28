8c1b01d121e6f17d6581e483a19e84fa0eaeac9a
github username <github email address>
2017-12-27 23:27:03 -0600

    correcting Notes spec

--- spec/models/note_spec.rb
+++ spec/models/note_spec.rb
@@ -2,33 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Note, type: :model do
   before(:each) do
-    3.times do
-      User.create(
-        username: Faker::StarWars.character,
-        password: Faker::StarWars.vehicle,
-        email: "#{Faker::Lovecraft.deity}@gmail.com"
-      )
-    end
-
-    3.times do
-      Freezer.create(
-        name: Faker::LordOfTheRings.location,
-        freezer_type: Faker::GameOfThrones.house
-      )
-    end
-
-    10.times do
-      ItemType.create(
-        title: Faker::Dune.character,
-        storage_min: "#{rand(1..4)}",
-        storage_max: "#{rand(5..10)}"
-      )
-    end
-
-    @item = User.first.items.create(title: "Hot Dogs", item_type_id: ItemType.first.id, freezer_id: Freezer.first.id)
-    @item2 = User.first.items.create(item_type_id: ItemType.second.id, freezer_id: Freezer.second.id)
-
-    @note = User.first.notes.create(item_id: @item.id)
+   @note = Note.first
   end
 
   it "belongs to a user" do
@@ -36,6 +10,6 @@ RSpec.describe Note, type: :model do
   end
 
   it "belongs to an item" do
-    expect(@note.item.title).to eq("Hot Dogs")
+    expect(@note.item.title).to eq("casserole")
   end
 end
