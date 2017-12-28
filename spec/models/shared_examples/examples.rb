88df611d36d7d964f373e72e5e60a848a570f80f
github username <github email address>
2017-12-27 22:32:20 -0600

    dynamic creation of dates stored

--- spec/models/shared_examples/examples.rb
+++ spec/models/shared_examples/examples.rb
@@ -11,22 +11,22 @@ Freezer.create(name: "office", freezer_type: "beer")
 Freezer.create(name: "garage", freezer_type: "french door")
 #5 item types
 ItemType.create(title: "frozen veggies", storage_min: "1", storage_max: "2")
-ItemType.create(title: "fruit pie", storage_min: "3", storage_max: "4")
-ItemType.create(title: "raw hamburger", storage_min: "5", storage_max: "6")
+ItemType.create(title: "fruit pie", storage_min: "2", storage_max: "4")
+ItemType.create(title: "raw hamburger", storage_min: "5", storage_max: "7")
 ItemType.create(title: "TV dinners", storage_max: "6")
-ItemType.create(title: "beef bones", storage_max: "12")
+ItemType.create(title: "beef bones", storage_max: "1")
 #10 items
-User.first.items.create(title: "casserole", item_type_id: 4, freezer_id: 1, date_stored: Time.now.months_ago(3))
-User.first.items.create(title: "beef stew", item_type_id: 3, freezer_id: 2, date_stored: "Sat, 29 Apr 2017".to_datetime)
+User.first.items.create(title: "casserole", item_type_id: 4, freezer_id: 1, date_stored: Time.now.months_ago(3))#still good
+User.first.items.create(title: "beef stew", item_type_id: 3, freezer_id: 2, date_stored: Time.now.months_ago(6)) #expiration_zone, 1 mo
 User.first.items.create(title: "pizza", item_type_id: 4, freezer_id: 3, date_stored: Time.now.months_ago(8)) #expired
-User.first.items.create(title: "cow parts", item_type_id: 5, freezer_id: 1, date_stored: "Wed, 18 Jan 2017".to_datetime)
-User.first.items.create(title: "apple pie", item_type_id: 2, freezer_id: 2, date_stored: "Thu, 14 Dec 2017".to_datetime)
+User.first.items.create(title: "cow parts", item_type_id: 5, freezer_id: 1, date_stored: Time.now.weeks_ago(1)) #still good
+User.first.items.create(title: "apple pie", item_type_id: 2, freezer_id: 2, date_stored: Time.now.months_ago(3)) #expiration zone, 1 mo
 User.first.items.create(title: "Manwich", item_type_id: 4, freezer_id: 3, date_stored: Time.now.months_ago(7)) #expired
-User.first.items.create(title: "beef patties", item_type_id: 3, freezer_id: 1, date_stored: "Mon, 14 Aug 2017".to_datetime)
-User.first.items.create(title: "peas", item_type_id: 1, freezer_id: 2, date_stored: "Sun, 08 Oct 2017".to_datetime)
-User.first.items.create(title: "cranberry pie", item_type_id: 2, freezer_id: 3, date_stored: "Sat, 21 Jan 2017".to_datetime)
-User.first.items.create(title: "broccoli", item_type_id: 1, freezer_id: 1, date_stored: "Fri, 13 Jan 2017".to_datetime)
-User.second.items.create(title: "broth", item_type_id: 5, freezer_id: 2, date_stored: "Dec. 31, 2016".to_datetime)
+User.first.items.create(title: "beef patties", item_type_id: 3, freezer_id: 1, date_stored: Time.now.months_ago(4)) #still good
+User.first.items.create(title: "peas", item_type_id: 1, freezer_id: 2, date_stored: Time.now.months_ago(3)) #expired
+User.first.items.create(title: "cranberry pie", item_type_id: 2, freezer_id: 3, date_stored: Time.now.months_ago(8)) #expired
+User.first.items.create(title: "broccoli", item_type_id: 1, freezer_id: 1, date_stored: Time.now.weeks_ago(2)) #still_good
+User.second.items.create(title: "broth", item_type_id: 5, freezer_id: 2, date_stored: Time.now.weeks_ago(3)) #in 1 week
 User.second.items.create(title: "curry", item_type_id: 4, freezer_id: 3, date_stored: Time.now.months_ago(5)) #expire in 1 mo
 #3 notes
 User.first.notes.create(content: "blahblah", item_id: 1)
