b9e34de890f35e7a352ce256c5cfb281054d4f96
github username <github email address>
2017-12-28 15:24:44 -0600

    created session routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,10 @@ Rails.application.routes.draw do
   root 'welcome#home'
   get '/auth/facebook/callback' => 'sessions#create'
   get '/auth/google/callback' => 'sessions#create'
-  
+
   resources :items
   resources :freezers
   resources :users
+  resources :sessions, only: [:new, :create, :destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
