5a91663a4f9fede87c9dcb7fffb4e4dc588e2e4c
github username <github email address>
2017-12-29 13:15:10 -0600

    debugging params in sessions#create

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -10,8 +10,8 @@ class SessionsController < ApplicationController
       session[:user_id] = user.id
       redirect_to user_home_path(user)
     else
-      user = User.find_by(username: params[:username])
-      if user && user.authenticate(params[:password])
+      user = User.find_by(email: params[:user][:email])
+      if user && user.authenticate(params[:user][:password])
         session[:user_id] = user.id
         redirect_to user_home_path(user)
       else
