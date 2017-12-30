fcde0469e832ce10d83b7267bcc6ae4c861a3244
github username <github email address>
2017-12-30 13:08:50 -0600

    added validations for ItemType

--- app/models/item_type.rb
+++ app/models/item_type.rb
@@ -1,5 +1,7 @@
 class ItemType < ApplicationRecord
   has_many :items#wired
   has_many :users, -> { distinct }, through: :items
+  validates :title, presence: true
+  validates :storage_max, presence: true
 
 end
