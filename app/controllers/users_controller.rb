1a51c3e9ed4e7d08229e3fb74fdf880f38d40fdd
github username <github email address>
2017-12-30 15:55:37 -0600

    helper method for rendering user errors

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,11 +17,11 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
+    binding.pry
     if @user.save
       session[:user_id] = @user.id
       redirect_to user_home_path(@user)
     else
-      #need to raise any errors
       render 'new'
     end
   end
--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -7,5 +7,13 @@ module UsersHelper
       "No"
     end
   end
-  
+
+  def user_errors(user)
+    if user.errors.any?
+      user.errors.full_messages.each do |message|
+        message
+      end
+    end
+  end
+
 end
