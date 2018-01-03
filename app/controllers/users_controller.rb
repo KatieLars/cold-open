87182a7dc55d909cbe4a5734a912cc0e4e492e81
github username <github email address>
2018-01-03 17:52:05 -0600

    finishing touches

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,8 +15,7 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
-  def create #done
-    binding.pry
+  def create
     @user = User.new(user_params)
     if @user.save
       session[:user_id] = @user.id
