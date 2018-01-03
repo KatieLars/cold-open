589bf6864fe02d37d10f19594862063b3b9368b8
github username <github email address>
2018-01-03 17:47:31 -0600

    sign up with facebook or google

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,6 +16,7 @@ class UsersController < ApplicationController
   end
 
   def create #done
+    binding.pry
     @user = User.new(user_params)
     if @user.save
       session[:user_id] = @user.id
