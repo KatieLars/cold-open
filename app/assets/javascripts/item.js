30d64c76e418f9d4fcd44043f169bfdaffb8c680
github username <github email address>
2018-02-12 15:38:25 -0600

    moving into assets

---
+++ app/assets/javascripts/item.js
@@ -0,0 +1,19 @@
+function getItems() {
+$("#items-link").on("click", function(){
+  var user = $("#items-link").data().userid
+  $.get("/users/"+user+"/items.json", function(response){
+      var data = response.data
+      var itemList = ""
+      data.forEach(item => {//eventually refactor into Item.prototype function ---may need to fuck with serializer to allow for expiration expiration_zone
+        itemList += '<a href="/users/'+user+'/items/'+item.id+'">'+item.attributes.title+':</a></br>'
+      })
+      debugger
+
+      console.log(itemList)
+    $("#items-list").html('<p>Hello World</p>')
+   })
+})
+}
+$(function() {
+    getItems()
+});
