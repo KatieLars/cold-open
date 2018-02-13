f071d1cdda09f7b061450fa400eef8af0a1a1d89
github username <github email address>
2018-02-12 21:30:59 -0600

    cleaned up notes

--- app/serializers/item_serializer.rb
+++ app/serializers/item_serializer.rb
@@ -16,17 +16,3 @@ class ItemSerializer < ActiveModel::Serializer
   end
 
 end
-
-# def format_date(date)
-#   if date
-#     date.strftime("%B %d, %Y")
-#   end
-# end
-#
-# def expiration_zone(expiration_min=0, expiration_max)
-#   if expiration_min
-#     "#{format_date(expiration_min)} - #{format_date(expiration_max)}"
-#   else
-#     "#{format_date(expiration_max)}"
-#   end
-# end
