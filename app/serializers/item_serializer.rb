5400461ca6f690b52d4e6851cdbe3cade970ea2e
github username <github email address>
2018-02-12 21:21:02 -0600

    serializer helper methods

--- app/serializers/item_serializer.rb
+++ app/serializers/item_serializer.rb
@@ -1,4 +1,33 @@
 class ItemSerializer < ActiveModel::Serializer
-  attributes :id, :title, :expiration_max, :expiration_min, :date_stored
+  attributes :id, :title, :expiration_max, :expiration_min, :date_stored, :expiration_zone
   #belongs_to :freezer
+
+  def expiration_zone
+    if object.expiration_min
+      "#{format_date(object.expiration_min)} - #{format_date(object.expiration_max)}"
+    else
+      "#{format_date(object.expiration_max)}"
+    end
+  end
+
+  def format_date(date)
+    if date
+     date.strftime("%B %d, %Y")
+    end
+  end
+
 end
+
+# def format_date(date)
+#   if date
+#     date.strftime("%B %d, %Y")
+#   end
+# end
+#
+# def expiration_zone(expiration_min=0, expiration_max)
+#   if expiration_min
+#     "#{format_date(expiration_min)} - #{format_date(expiration_max)}"
+#   else
+#     "#{format_date(expiration_max)}"
+#   end
+# end
