3a1beb749d2f58c3f1161dd0b26b6dc9b50a69a4
github username <github email address>
2018-02-13 14:51:16 -0600

    helper methods in note serializer

--- app/serializers/note_serializer.rb
+++ app/serializers/note_serializer.rb
@@ -1,4 +1,20 @@
 class NoteSerializer < ActiveModel::Serializer
   attributes :id, :updated_at, :content, :created_at
   belongs_to :item
+
+  def format_date(date) #put this in concerns
+    if date
+     date.strftime("%B %d, %Y")
+    end
+  end
+
+  def create_or_updated_at
+    if object.updated_at
+      "<p><strong>Created at: </strong>#{format_date(object.created_at)}</p>
+        <p><strong>Last Updated at: </strong>#{format_date(object.updated_at)}</p>"
+    else
+      "<p><strong>Created at: </strong>#{format_date(object.created_at)}</p>"
+    end
+  end
+
 end
